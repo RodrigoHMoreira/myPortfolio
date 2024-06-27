2fc5cf31d283687c6ba146f51a2e897b4afeae2f
Rodrigo Moreira <73905577+RodrigoHMoreira@users.noreply.github.com>
2024-06-27 08:26:03 -0300

    Update index.tsx
    ...

--- templates/habilidades/index.tsx
+++ templates/habilidades/index.tsx
@@ -98,8 +98,7 @@ const Habilidades = () => {
           <ul>
             <li>
               <a href="https://www.w3.org/html/" target="_blank">
-                <div>
-                </div>
+              <h2>HTML</h2>
                 <span>
                   HTMLé uma linguagem de marcação utilizada na construção de
                   páginas na Web.
@@ -111,6 +110,7 @@ const Habilidades = () => {
                 href="https://www.w3.org/Style/CSS/Overview.en.html"
                 target="_blank"
               >
+                       <h2>CSS</h2>
                 <span>
                   CSS é um mecanismo para adicionar estilo a um documento Web.
                 </span>
@@ -118,9 +118,7 @@ const Habilidades = () => {
             </li>
             <li>
               <a href="https://www.javascript.com/" target="_blank">
-                <div>
-                  <JSImage />
-                </div>
+                     <h2>JAVASCRIPT</h2>
                 <span>
                   JavaScript é uma linguagem de programação para itens complexos
                   em páginas Web
@@ -129,6 +127,7 @@ const Habilidades = () => {
             </li>
             <li>
               <a href="https://www.typescriptlang.org/" target="_blank">
+                  <h2>TYPECRIPT</h2>
                 <span>
                   TypeScript é um superconjunto JavaScript que adicona tipagem
                   na linguagem.
@@ -137,6 +136,7 @@ const Habilidades = () => {
             </li>
             <li>
               <a href="https://pt-br.reactjs.org/" target="_blank">
+                  <h2>REACT</h2>
                 <span>
                   React é uma biblioteca JavaScript que cria interfaces de
                   usuário em páginas Web.
@@ -145,6 +145,7 @@ const Habilidades = () => {
             </li>
             <li>
               <a href="https://nextjs.org/" target="_blank">
+                  <h2>NEXT.JS</h2>
                 <span>
                   Next.js é um framework de para a criação aplicações front-end
                   e back-end.
@@ -153,6 +154,7 @@ const Habilidades = () => {
             </li>
             <li>
               <a href="https://nodejs.org/en/" target="_blank">
+                  <h2>NODE.JS</h2>
                 <span>
                   Node.js é um ambiente de execução que permite executar
                   aplicações sem um navegador.
@@ -161,6 +163,7 @@ const Habilidades = () => {
             </li>
             <li>
               <a href="https://git-scm.com/" target="_blank">
+                  <h2>GIT</h2>
                 <span>
                   Git é um sistema de controle de versões distribuído, usado
                   para desenvolver software.
@@ -169,6 +172,7 @@ const Habilidades = () => {
             </li>
               <li>
               <a href="https://www.mongodb.com/" target="_blank">
+                  <h2>MONGO DB</h2>
                 <span>
                 MongoDB é um banco de dados NoSQL que armazena dados em documentos JSON.
                 </span>
@@ -176,6 +180,7 @@ const Habilidades = () => {
             </li>  
             <li>
               <a href="https://www.mysql.com/" target="_blank">
+                  <h2>MYSQL</h2>
                 <span>
                  MySQL é um banco de dados relacional popular que utiliza SQL para gerenciar dados.
                 </span>
@@ -183,6 +188,7 @@ const Habilidades = () => {
             </li>  
             <li>
               <a href="https://www.prisma.io/" target="_blank">
+                  <h2>PRISMA</h2>
                 <span>
                  Prisma é um ORM que simplifica a interação com bancos de dados SQL e NoSQL.
                 </span>
@@ -190,6 +196,7 @@ const Habilidades = () => {
             </li>  
             <li>
               <a href="https://jestjs.io/" target="_blank">
+                  <h2>JEST</h2>
                 <span>
                   Jest é um framework de teste em JavaScript, usado para testar aplicações front-end e back-end.
                 </span>
